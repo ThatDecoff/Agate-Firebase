60021b67fa62f1426f3b60b75d7d118424ad5c7d
Adrian Kaiser <adriankaiser.rian@gmail.com>
2021-11-01 16:03:21 +0700

    added analytic functions

--- Incremental Game Firebase/Assets/Scripts/Save Game/UserDataManager.cs
+++ Incremental Game Firebase/Assets/Scripts/Save Game/UserDataManager.cs
@@ -82,6 +82,8 @@ public static class UserDataManager
 
         if (uploadToCloud)
         {
+            AnalyticsManager.SetUserProperties("gold", Progress.Gold.ToString());
+
             byte[] data = Encoding.Default.GetBytes(json);
 
             StorageReference targetStorage = GetTargetCloudStorage();
